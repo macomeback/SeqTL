9ef862405fc2c2c982f69e69dd289dc138bc89fa
macomeback <matinamini1991@yahoo.com>
2025-12-15 14:55:08 -0800

    Video data read

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ from lark import Lark
 from prop_builder import PropBuilder
 from matcher import Matcher
 from oracle import ShapeExpressionOracle
+import os
+
+def read_boxes(file_path:str) -> list[set[list[float]]]:
+    trace = []
+    f = open(file_path, 'r')
+    for line in f:
+        boxes = set()
+        for box in line.split():
+            boxes.add(box.split(',')[:-1])
+        trace.append(boxes)
+    return trace
 
 parser = Lark(r"""
     start: OPENBASE SIGNED_NUMBER DASH SIGNED_NUMBER CLOSEBASE
@@ -32,13 +43,15 @@ parser = Lark(r"""
 
     """, start='start')
 
-parsed_tree = parser.parse("[1-1]*[10-361]^<1,361,l-0.1-.-.-0-.>[1-1]*")
+#parsed_tree = parser.parse("[1-1]*[10-361]^<1,361,l-0.1-.-.-0-.>[1-1]*")
+parsed_tree = parser.parse("[1-1]*[24-40]^<24,40,close>[24-40][24-40]^<24,40,far>")
 builder = PropBuilder(parsed_tree)
 prop, query_map = builder.build_prop()
 #print(parsed_tree.pretty())
-f = open('418_C_BBB_101_9s_full.csv', 'r')
-trace = []
-for line in f:
-    trace.append(float(line))
-matcher = Matcher(prop, ShapeExpressionOracle(query_map), trace)
-print(matcher.match())
+#f = open('418_C_BBB_101_9s_full.csv', 'r')
+dir_path = '../prep-charades'
+for file_name in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, file_name)
+        trace = read_boxes(file_path)
+        matcher = Matcher(prop, ShapeExpressionOracle(query_map), trace)
+        print(matcher.match())
