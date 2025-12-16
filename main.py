fd74024d1992db59a8ee98d0c7967cd7247df117
macomeback <matinamini1991@yahoo.com>
2025-12-15 16:14:20 -0800

    Boxes size

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def read_boxes(file_path:str) -> list[list[list[float]]]:
     for line in f:
         boxes = []
         for box in line.split():
-            boxes.append(box.split(',')[:-1])
+            boxes.append([x for x in box.split(',') if len(x)>0])
+            assert len(boxes[-1]) == 6
         trace.append(boxes)
     return trace
 
